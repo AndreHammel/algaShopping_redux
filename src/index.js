e056e692a3d94da42e6a7689068e35ded751d948
AndreHammel <hammel.engineer@gmail.com>
2022-05-09 20:48:15 -0300

    configurado o redux persist

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomeView from './views/Home.view';
-import store from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistedStore } from './store/store'
 import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={ store }>
-      <HomeView />
+      <PersistGate persistor={ persistedStore }>
+        <HomeView />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
